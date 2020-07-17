8731078189f71052bacf83b535fca55d50cbba70
linglingsyu <zzxcv741@hotmail.com>
2020-07-17 15:11:52 +0800

    顯示場次時間 有bug

---
+++ api/get_duration.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once "../base.php";
+
+$db = new DB("movie");
+$movie = $db -> find($_GET['id']);
+$today = strtotime(date("Y-m-d"));
+$ondate = strtotime($movie['ondate']);
+
+for ($i = 0; $i < 3; $i++){
+  $chk = strtotime("+$i days",$ondate); 
+  if( $chk >= $today ){
+    //要顯示出來的
+    echo '<option value="'.date("m月d日 l",$chk).'">'.date("Y-m-d",$chk).'</option>';
+  }
+}
+
+
+?>
---
+++ api/get_session.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once "../base.php";
+$sess = [
+  1=>"14:00~16:00",
+  2=>"16:00~18:00",
+  3=>"18:00~20:00",
+  4=>"20:00~22:00",
+  5=>"22:00~24:00",
+];
+
+// $db = new DB("movie");
+// $movie = $db -> find($_GET['id']);
+$today = strtotime(date("Y-m-d"));
+// $ondate = strtotime($movie['ondate']);
+
+if( strtotime($_GET['date']) == $today ){
+  $now = floor((date("G")-12)/2); // 現在電影在播放第幾場
+  for($i = ($now+1) ; $i <= 5 ; $i++ ){
+    //要顯示的場次從下一場開始算
+    echo '<option value="'.$i.'">'.$sess[$i].'</option>';
+  }
+
+}else{
+
+    for($i = 1; $i<=5;$i++){
+      echo '<option value="'.$i.'">'.$sess[$i].'</option>';
+    } 
+}
+
+
+?>
+
