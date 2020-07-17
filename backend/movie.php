7c47554dfc5891393559513d4c956f3717d243a5
linglingsyu <zzxcv741@hotmail.com>
2020-07-17 09:02:42 +0800

    完成院線片排序及刪除功能

--- backend/movie.php
+++ backend/movie.php
@@ -43,6 +43,9 @@
 $db = new DB('movie');
 $rows = $db->all([]," order by `rank`");
 foreach($rows as $k=>$row){
+  $prev = ($k != 0)?$rows[$k-1]['id']:$row['id'];      
+  //如果不是最後一筆，顯示下一筆的id，如果是最後一筆，顯示自己的id
+  $next = ($k != (count($rows)-1))?$rows[$k+1]['id']:$row['id'];
 ?>
 <div class="movie-item">
   <div style="line-height:100%"><img src="img/<?=$row['poster'] ?>" style="width:80px;height:100px;"></div>
@@ -53,7 +56,13 @@ foreach($rows as $k=>$row){
       <span>片長:<?=$row['length'] ?></span>
       <span>上映時間:<?=$row['ondate'] ?></span>
     </div>
-    <div>功能按鈕</div>
+    <div>
+    <button onclick="sh('movie',<?= $row['id'] ?>)">顯示</button>
+    <button data-rank="<?= $row['id']."-". $prev ?>" class="shift">往上</button>
+    <button data-rank="<?= $row['id']."-". $next ?>" class="shift">往下</button>
+    <button onclick="edit('movie',<?= $row['id'] ?>)">編輯電影</button>
+    <button onclick="del('movie',<?= $row['id'] ?>)">刪除電影</button>
+  </div>
     <div>劇情簡介:<?=$row['intro'] ?></div>
   </div>
 </div>
@@ -64,3 +73,20 @@ foreach($rows as $k=>$row){
 
 </div>
 
+<script>
+
+  $(".shift").on("click",function(){
+    let id = $(this).data("rank").split("-");
+    //我點下的那個按鈕的data-rank的值用-分開後，轉成陣列存放
+    $.post("api/rank.php",{id,"table":"movie"},function(res){
+      location.reload();
+    })
+  })
+
+  function del(table,id){
+    $.post("api/del.php",{"table":table,id},function(){
+      location.reload();
+    })
+  }
+
+</script>
