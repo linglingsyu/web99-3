95d15f19817c11ff406eb49131f7257c4a1b7d76
linglingsyu <zzxcv741@hotmail.com>
2020-07-24 09:18:37 +0800

    完成挑選座位畫面

--- api/get_session.php
+++ api/get_session.php
@@ -15,7 +15,8 @@ $today = strtotime(date("Y-m-d"));
 // $ondate = strtotime($movie['ondate']);
 
 if( strtotime($_GET['date']) == $today ){
-  $now = floor((date("G")-12)/2); // 現在電影在播放第幾場
+  $now = floor((date("G")-12)/2); // 現在電影在播放第幾場  (早上時10-12 = -2 會有負值的問題)
+  $now = ($now > 0) ? $now  :  0 ;//修正負值 . 早上訂票顯示問題 (考試不用打) 
   for($i = ($now+1) ; $i <= 5 ; $i++ ){
     //要顯示的場次從下一場開始算
     echo '<option value="'.$i.'">'.$sess[$i].'</option>';
--- front/order.php
+++ front/order.php
@@ -1,3 +1,4 @@
+<div class="order_form">
 <form>
 <h3 class="ct">線上訂票</h3>
 <table style="width:70%;margin:auto;">
@@ -32,8 +33,51 @@ foreach ($rows as $row ){
     <td><select name="session" id="session"></select></td>
   </tr>
 </table>
-<div class="ct"><input type="button" value="確定"><input type="reset" value="重置"></div>
+<div class="ct"><input type="button" value="確定" onclick="booking()"><input type="reset" value="重置"></div>
 </form>
+</div>
+
+<style>
+  .room{
+    width:320px;
+    height:320px;
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .room > div {
+    width:64px;
+    height:80px;
+    position: relative;
+    background-color: lightskyblue;
+  }
+
+  .room > div:nth-child(odd){
+    background-color: lightsalmon;
+  }
+</style>
+
+<div class="booking_form" style="display:none;">
+
+  <div class="room">
+      <?php
+      for($i=0 ; $i<20; $i++){
+        echo "<div>";
+        echo floor($i/5)+1;
+        echo "排";
+        echo ($i%5)+1;
+        echo "號";
+        echo "</div>";
+      }
+
+      ?>
+
+  </div>
+
+
+<button onclick="prev();">上一步</button>
+
+</div>
 
 <script>
 
@@ -63,5 +107,17 @@ function getSession(){
   })
 }
 
+
+//挑選座位
+function booking(){
+  $(".order_form").hide();
+  $(".booking_form").show();
+}
+
+//上一步
+function prev(){
+  $(".order_form").show();
+  $(".booking_form").hide();
+}
 
 </script>
