0c5b91af9ea612b1bbcdf1156f2e703f3efc6439
linglingsyu <zzxcv741@hotmail.com>
2020-07-29 09:22:46 +0800

    完成刪除單筆資料功能

--- backend/order.php
+++ backend/order.php
@@ -1,77 +1,91 @@
 <h3 class="ct">訂單清單</h3>
 <div>快速刪除：
-<input type="radio" name="bydate" id="bydate">依日期<input type="text" name="date" id="date">
-<input type="radio" name="bymovie" id="bymovie">
-<select name="movie" id="movie">
-<?php
+  <input type="radio" name="bydate" id="bydate">依日期<input type="text" name="date" id="date">
+  <input type="radio" name="bymovie" id="bymovie">
+  <select name="movie" id="movie">
+    <?php
 
-$mlist = $Ord->all([]," group by `movie`");
+    $mlist = $Ord->all([], " group by `movie`");
 
-foreach($mlist as $m){
-?>
+    foreach ($mlist as $m) {
+    ?>
 
-<option value="<?= $m['movie'] ?>"><?= $m['movie'] ?></option>
+      <option value="<?= $m['movie'] ?>"><?= $m['movie'] ?></option>
 
-<?php
-}
-?>
+    <?php
+    }
+    ?>
 
-</select>
-<button>刪除</button>
+  </select>
+  <button>刪除</button>
 </div>
 
 <div>
   <style>
-    ul{
+    ul {
       display: flex;
       list-style-type: none;
-      padding:0;
+      padding: 0;
     }
-    ul li{
-      width:14%;
+
+    ul li {
+      width: 14%;
     }
   </style>
   <ul style="background-color: #eee;">
-  <li>訂單編號</li>
-  <li>電影名稱</li>
-  <li>日期</li>
-  <li>場次時間</li>
-  <li>訂購數量</li>
-  <li>訂購位置</li>
-  <li>操作</li>
-</ul>
-<div style="overflow:auto;height:250px">
-<?php
-$orders = $Ord->all([]," order by no desc");
-foreach($orders as $ord){
-?>
-<ul>
-  <li><?= $ord['no'] ?></li>
-  <li><?= $ord['movie'] ?></li>
-  <li><?= $ord['date'] ?></li>
-  <li><?= $ord['session'] ?></li>
-  <li><?= $ord['qt'] ?></li>
-  <li><?php 
-  
- $seat = unserialize( $ord['seat'] );
- foreach($seat as $s){
-   echo floor($s/5)+1;
-   echo "排";
-   echo $s%5+1;
-   echo "號";
-   echo "<br>";
- }
-  
-  ?></li>
-  <li><button>刪除</button></li>
-</ul>
-<hr>
+    <li>訂單編號</li>
+    <li>電影名稱</li>
+    <li>日期</li>
+    <li>場次時間</li>
+    <li>訂購數量</li>
+    <li>訂購位置</li>
+    <li>操作</li>
+  </ul>
+  <div style="overflow:auto;height:250px">
+    <?php
+    $orders = $Ord->all([], " order by no desc");
+    foreach ($orders as $ord) {
+    ?>
+      <ul>
+        <li><?= $ord['no'] ?></li>
+        <li><?= $ord['movie'] ?></li>
+        <li><?= $ord['date'] ?></li>
+        <li><?= $ord['session'] ?></li>
+        <li><?= $ord['qt'] ?></li>
+        <li><?php
+
+            $seat = unserialize($ord['seat']);
+            foreach ($seat as $s) {
+              echo floor($s / 5) + 1;
+              echo "排";
+              echo $s % 5 + 1;
+              echo "號";
+              echo "<br>";
+            }
+
+            ?></li>
+        <li><button onclick="del('ord',<?= $ord['id'] ?>)">刪除</button></li>
+      </ul>
+      <hr>
 
 
-<?php
-}
-?>
+    <?php
+    }
+    ?>
+
+  </div>
 
 </div>
 
-</div>
+<script>
+  function del(table, id) {
+    if (confirm("你確定要刪除此筆資料嗎?")) {
+      $.post("api/del.php", {
+        "table": table,
+        id
+      }, function() {
+        location.reload();
+      })
+    }
+  }
+</script>
